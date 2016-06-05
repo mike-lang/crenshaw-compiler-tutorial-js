6d9c5c979eab23ab5e38c23113cc2eb409a233b3
Mike Lang <mike.lang1010@gmail.com>
2016-06-05 18:48:53 -0500

    Temporary files implementing multi-character tokens/values

---
+++ cradle_b.js
@@ -0,0 +1,165 @@
+'use strict';
+
+const q = require('q');
+
+const TAB = '\t';
+
+let look;
+exports.look = function() {
+  return look;
+};
+
+function getChar() {
+  const stdin = process.stdin;
+  const deferred = q.defer();
+
+  stdin.setRawMode(true);
+  stdin.resume();
+  stdin.once('data', function(c) {
+    if ( c.toString() === '\u0003' ) {
+      process.exit();
+    }
+    stdin.pause();
+    stdin.setRawMode(false);
+    look = c.toString();
+    stdin.removeAllListeners('error');
+    return deferred.resolve();
+  });
+  stdin.once('error', function(err) {
+    return deferred.reject(err);
+  });
+
+  return deferred.promise;
+}
+exports.getChar = getChar;
+
+function error(err) {
+  console.log('\nError: ' + err + '.');
+  console.log(new Error().stack);
+}
+exports.error = error;
+
+function abort(err) {
+  error(err);
+  process.exit(1);
+}
+exports.abort = abort;
+
+function expected(s) {
+  abort(s + ' Expected');
+}
+exports.expected = expected;
+
+function match(x) {
+  if (look === x) return getChar();
+  else expected(`''` + x + `''`);
+}
+exports.match = match;
+
+function isAlpha(c) {
+  return !!c.match(/[a-zA-Z]/);
+}
+exports.isAlpha = isAlpha;
+
+function isDigit(c) {
+  return !!c.match(/\d/);
+}
+exports.isDigit = isDigit;
+
+function isAlNum(c) {
+  return isAlpha(c) || isDigit(c);
+}
+exports.isAlNum = isAlNum;
+
+function getNameInner(token) {
+  let nextChar = look;
+  if (isAlNum(nextChar)) {
+    token = token + nextChar.toUpperCase();
+    return getChar()
+      .then(() => {
+        return getNameInner(token);
+      });
+  } else {
+    return token;
+  }
+}
+
+function getName() {
+  if (!isAlpha(look)) expected('Name');
+  return q()
+    .then(() => {
+      return getNameInner('');
+    });
+}
+exports.getName = getName;
+
+function getNumInner(value) {
+  let nextChar = look;
+  if (isDigit(nextChar)) {
+    value = value + nextChar;
+    return getChar()
+      .then(() => {
+        return getNumInner(value);
+      });
+  } else {
+    return value;
+  }
+}
+
+function getNum() {
+  if (!isDigit(look)) expected('Integer');
+  return q()
+    .then(() => {
+      return getNumInner('');
+    });
+}
+exports.getNum = getNum;
+
+
+function emit(s) {
+  process.stdout.write(TAB + s);
+}
+exports.emit = emit;
+
+
+function asmHeader() {
+  // This next line came from the EASy68K boilerplate
+  // look up what it means
+  emitLn('ORG\t$1000'); 
+  label('START');
+}
+function label(s) {
+  process.stdout.write(s + ':\n');
+}
+
+function emitLn(s) {
+  emit(s);
+  process.stdout.write('\n');
+}
+exports.emitLn = emitLn;
+
+function simHalt() {
+  emitLn('SIMHALT');
+}
+
+function asmFooter() {
+  emitLn('END\tSTART');
+}
+
+function init() {
+  asmHeader();
+  return getChar();
+}
+exports.init = init;
+
+function finish() {
+  asmFooter();
+}
+exports.finish = finish;
+
+/** skeletal main program */
+function main() {
+  return init()
+    .then(() => {
+    });
+}
---
+++ expressions_b.js
@@ -0,0 +1,209 @@
+'use strict';
+
+process.on('unhandledException', function(err) {
+  console.log(err.stack);
+});
+
+const CR = '\r';
+
+const cradle = require('./cradle_b'),
+  q = require('q');
+
+const emitLn = cradle.emitLn,
+  getNum = cradle.getNum,
+  getName = cradle.getName,
+  init = cradle.init,
+  finish = cradle.finish,
+  match = cradle.match,
+  look = cradle.look,
+  abort = cradle.abort,
+  isAlpha = cradle.isAlpha,
+  isAlNum = cradle.isAlnum,
+  expected = cradle.expected;
+
+function ident() {
+  return getName()
+    .then((name) => {
+      let nextChar = look();
+      if (nextChar === '(') {
+        return match('(')
+          .then(() => {
+            return match(')');
+          })
+          .then(() => {
+            emitLn('BSR ' + name);
+          });
+      } else {
+        emitLn('MOVE ' + name + '(PC),D0');
+      }
+    });
+}
+
+function factor() {
+  let nextChar = look();
+  if (nextChar === '(') {
+    return match('(')
+      .then(() => {
+        return expression();
+      })
+      .then(() => {
+        return match(')');
+      });
+  } else if (isAlpha(nextChar)) {
+    return ident();
+  } else {
+    return getNum()
+      .then((num) => {
+        emitLn('MOVE #' + num + ',D0');
+      });
+  }
+}
+
+function term() {
+  return factor()
+    .then(() => {
+      return termTail();
+    });
+}
+
+function multiply() {
+  return match('*')
+    .then(() => {
+      return factor()
+    })
+    .then(() => {
+      emitLn('MULS (SP)+,D0');
+    });
+}
+
+function divide() {
+  return match('/')
+    .then(() => {
+      return factor()
+    })
+    .then(() => {
+      emitLn('MOVE (SP)+,D1');
+      emitLn('DIVS D1,D0');
+    });
+}
+function termTail() {
+  let nextChar = look();
+  if (nextChar === '*' || nextChar === '/') {
+    emitLn('MOVE D0,-(SP)');
+    return q()
+      .then(() => {
+        let nextChar = look();
+        switch(nextChar) {
+          case '*':
+            return multiply();
+          case '/':
+            return divide();
+        }
+      })
+      .then(() => {
+        return termTail();
+      });
+  } else {
+    return;
+  }
+}
+
+function isAddOp(c) {
+  return (c === '+' || c === '-')
+}
+
+function isAlNum(c) {
+  return isAlpha(c) || isDigit(c);
+}
+
+function add() {
+  return match('+')
+    .then(() => {
+      return term();
+    })
+    .then(() => {
+      emitLn('ADD (SP)+,D0');
+    });
+}
+
+function subtract() {
+  return match('-')
+    .then(() => {
+      return term()
+    })
+    .then(() => {
+      emitLn('SUB (SP)+,D0');
+      emitLn('NEG D0');
+    });
+}
+
+function expression() {
+  let nextChar = look();
+  return q()
+    .then(() => {
+      if (isAddOp(nextChar)) {
+        emitLn('CLR D0')
+      } else {
+        return term();
+      }
+    })
+    .then(() => {
+      return expressionTail();
+    });
+}
+
+function expressionTail() {
+  let nextChar = look();
+  if (isAddOp(nextChar)) {
+    emitLn('MOVE D0,-(SP)');
+    return q()
+      .then(() => {
+        let nextChar = look();
+        switch(nextChar) {
+          case '+':
+            return add();
+          case '-':
+            return subtract();
+        }
+      })
+      .then(() => {
+        return expressionTail();
+      });
+  } else {
+    return;
+  }
+}
+
+function assignment() {
+  // Somewhere we need to generate DS directives
+  // for this to work with the EASy68K assembler.
+  // Leaving this as an exercise for now to collect variables
+  // and collect generated code in a buffer so that
+  // the appropriate header with symbol definitions
+  // is put before the code in the results emitted
+  // on stdout
+  return getName()
+    .then((name) => {
+      return match('=')
+        .then(() => {
+          return expression()
+            .then(() => {
+              emitLn('LEA ' + name + '(PC),A0');
+              emitLn('MOVE D0,(A0)');
+            });
+        });
+    });
+}
+
+return init()
+  .then(() => {
+    return assignment();
+  })
+  .then(() => {
+    let nextChar = look();
+    if (nextChar !== CR) expected('Newline');
+    return finish();
+  })
+  .catch((err) => {
+    abort(err.stack);
+  });
