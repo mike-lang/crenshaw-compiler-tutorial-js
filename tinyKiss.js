55e20669947da9978e233b1ff4fb5b0e1548be43
Mike Lang <mike.lang1010@gmail.com>
2016-07-17 16:51:43 -0500

    tinyKiss 0.1

--- tinyKiss.js
+++ tinyKiss.js
@@ -11,12 +11,42 @@ const match = cradle.match,
   getChar = cradle.getChar,
   getName = cradle.getName,
   isDigit = cradle.isDigit,
+  isAlpha = cradle.isAlpha,
   expected = cradle.expected;
 
 
 const CR = '\r';
 
 let symbolTable = {};
+
+let labelCount = 0;
+
+function newLabel() {
+  let result = 'L' + labelCount;
+  labelCount++;
+  return result;
+}
+
+function postLabel(l) {
+  console.log(l + ':');
+}
+
+function isOrop(c) {
+  return c === '|' || c ==='~';
+}
+
+function isRelop(c) {
+  return c === '=' || c === '#' || c === '<' || c === '>';
+}
+
+function isMulop(c) {
+  return c === '*' || c === '/';
+}
+
+function isAddop(c) {
+  return c === '+' || c === '-';
+}
+
 
 function getNum() {
   function getNumTail(value) {
@@ -84,6 +114,9 @@ function main() {
   return match('b')
     .then(() => {
       return prolog();
+    })
+    .then(() => {
+      return block();
     })
     .then(() => {
       return match('e');
@@ -179,6 +212,547 @@ function alloc(name) {
 
 function inTable(name) {
   return symbolTable[name] !== undefined;
+}
+
+function assignment() {
+  return getName()
+    .then((name) => {
+      return match('=')
+        .then(() => {
+          return boolExpression();
+        })
+        .then(() => {
+          return store(name);
+        });
+    });
+}
+
+function block() {
+  let nextChar = look();
+  if (nextChar !== 'e' && nextChar !== 'l') {
+    return q()
+      .then(() => {
+        switch(nextChar) {
+          case 'i': return doIf();
+          case 'w': return doWhile();
+          default: return assignment();
+        }
+      })
+      .then(() => {
+        return block();
+      });
+  }
+}
+
+// Clears the primary register
+function clear() {
+  emitLn('CLR D0');
+}
+
+// Negates the primary register
+function negate() {
+  emitLn('NEG D0');
+}
+
+// Load a constant value to primary register
+function loadConst(n) {
+  emitLn(`MOVE #${n},D0`);
+}
+
+// Load a variable to primary register
+function loadVar(name) {
+  if (!inTable(name)) {
+    undefinedVar(name);
+  } 
+  emitLn(`MOVE ${name}(PC),D0`);
+}
+
+// Push primary register onto stack
+function push() {
+  emitLn('MOVE D0,-(SP)');
+}
+
+// Add top of stack to primary register
+function popAdd() {
+  emitLn('ADD (SP)+,D0');
+}
+
+// Subtract primary register from top of stack
+function popSub() {
+  emitLn('SUB (SP)+,D0');
+  emitLn('NEG D0');
+}
+
+// Multiply top of stack by primary register
+function popMul() {
+  emitLn('MULS (SP)+,D0');
+}
+
+// Divide top of stack by primary register
+function popDiv() {
+  emitLn('MOVE (SP)+,D7');
+  emitLn('EXT.L D7');
+  emitLn('DIVS D0,D7');
+  emitLn('MOVE D7,D0');
+}
+
+// Store primary register to variable
+function store(name) {
+  if (!inTable(name)) {
+    undefinedVar(name);
+  }
+  emitLn(`LEA ${name}(PC),A0`);
+  emitLn('MOVE D0,(A0)');
+}
+
+// Complement the primary register
+function notIt() {
+  emitLn('NOT D0');
+}
+
+// AND top of stack with primary register
+function popAnd() {
+  emitLn('AND (SP)+,D0');
+}
+
+// OR top of stack with primary register
+function popOr() {
+  emitLn('OR (SP)+,D0');
+}
+
+// XOR top of stack with primary register
+function popXor() {
+  emitLn('EOR (SP)+,D0');
+}
+
+// Compare top of stack with primary register
+function popCompare() {
+  emitLn('CMP (SP)+,D0');
+}
+
+// Set D0 if compare was =
+function setEqual() {
+  emitLn('SEQ D0');
+  emitLn('EXT D0');
+}
+
+// Set D0 if Compare was !=
+function setNEqual() {
+  emitLn('SNE D0');
+  emitLn('EXT D0');
+}
+
+// Set D0 if compare was >
+function setGreater() {
+  emitLn('SLT D0');
+  emitLn('EXT D0');
+}
+
+// Set D0 if compare was <
+function setLess() {
+  emitLn('SGT D0');
+  emitLn('EXT D0');
+}
+
+// Branch unconditional
+function branch(l) {
+  emitLn('BRA ' + l);
+}
+
+// Branch false
+function branchFalse(l) {
+  emitLn('TST D0');
+  emitLn('BEQ ' + l);
+}
+
+function equals() {
+  return match('=')
+    .then(() => {
+      return expression();
+    })
+    .then(() => {
+      return popCompare();
+    })
+    .then(() => {
+      return setEqual();
+    });
+}
+
+function notEquals() {
+  return match('#')
+    .then(() => {
+      return expression();
+    })
+    .then(() => {
+      return popCompare();
+    })
+    .then(() => {
+      return setNEqual();
+    });
+}
+
+function less() {
+  return match('<')
+    .then(() => {
+      return expression();
+    })
+    .then(() => {
+      return popCompare();
+    })
+    .then(() => {
+      return setLess();
+    });
+}
+
+function greater() {
+  return match('>')
+    .then(() => {
+      return expression();
+    })
+    .then(() => {
+      return popCompare();
+    })
+    .then(() => {
+      return setGreater();
+    });
+}
+
+function relation() {
+  return expression()
+    .then(() => {
+      let nextChar = look();
+      if (isRelop(nextChar)) {
+        return q()
+          .then(() => {
+            return push();
+          })
+          .then(() => {
+            switch(nextChar) {
+              case '=': return equals();
+              case '#': return notEquals();
+              case '<': return less();
+              case '>': return greater();
+            }
+          })
+      }
+    })
+}
+
+function notFactor() {
+  let nextChar = look();
+  if (nextChar === '!') {
+    return match('!')
+      .then(() => {
+        return relation();
+      })
+      .then(() => {
+        return notIt();
+      });
+  } else {
+    return relation();
+  }
+}
+
+function boolTerm() {
+  function boolTermTail() {
+    let nextChar = look();
+    if (nextChar === '&') {
+      return q()
+        .then(() => {
+          return push();
+        })
+        .then(() => {
+          return match('&');
+        })
+        .then(() => {
+          return notFactor();
+        })
+        .then(() => {
+          return popAnd();
+        })
+        .then(() => {
+          return boolTermTail();
+        });
+    }
+  }
+
+  return notFactor()
+    .then(() => {
+      return boolTermTail();
+    });
+}
+
+function boolOr() {
+  return match('|')
+    .then(() => {
+      return boolTerm();
+    })
+    .then(() => {
+      return popOr();
+    });
+}
+
+function boolXor() {
+  return match('~')
+    .then(() => {
+      return boolTerm();
+    })
+    .then(() => {
+      return popXor();
+    });
+}
+
+function boolExpression() {
+  function boolExpressionTail() {
+    let nextChar = look();
+    if (isOrop(nextChar)) {
+      return q()
+        .then(() => {
+          return push();
+        })
+        .then(() => {
+          switch(nextChar) {
+            case '|': return boolOr();
+            case '~': return boolXor();
+          }
+        })
+        .then(() => {
+          return boolExpressionTail();
+        });
+    }
+  }
+
+  return boolTerm()
+    .then(() => {
+      return boolExpressionTail();
+    });
+}
+
+function undefinedVar(name) {
+  abort('Undefined Identifier ' + name);
+}
+
+function factor() {
+  let nextChar = look();
+  if (nextChar === '(') {
+    return match('(')
+      .then(() => {
+        return boolExpression();
+      })
+      .then(() => {
+        return match(')');
+      });
+  } else if (isAlpha(nextChar)) {
+    return getName()
+      .then((name) => {
+        return loadVar(name);
+      });
+  } else {
+    return getNum()
+      .then((number) => {
+        return loadConst(number);
+      });
+  }
+}
+
+function negFactor() {
+  return match('-')
+    .then(() => {
+      let nextChar = look();
+      if (isDigit(nextChar)) {
+        return getNum()
+          .then((number) => {
+            return loadConst(number * -1);
+          });
+      } else {
+        return factor()
+          .then(() => {
+            return negate();
+          });
+      }
+    });
+}
+
+function firstFactor() {
+  let nextChar = look();
+  switch(nextChar) {
+    case '+':
+      return match('+')
+        .then(() => {
+          return factor();
+        });
+    case '-':
+      return negFactor();
+    default:
+      return factor();
+  }
+}
+
+function multiply() {
+  return match('*')
+    .then(() => {
+      return factor();
+    })
+    .then(() => {
+      return popMul();
+    });
+}
+
+function divide() {
+  return match('/')
+    .then(() => {
+      return factor();
+    })
+    .then(() => {
+      return popDiv();
+    });
+}
+
+function term1() {
+  let nextChar = look();
+  if (isMulop(nextChar)) {
+    return q()
+      .then(() => {
+        return push();
+      })
+      .then(() => {
+        let nextChar = look();
+        switch(nextChar) {
+          case '*': return multiply();
+          case '/': return divide();
+        }
+      })
+      .then(() => {
+        return term1();
+      });
+  }
+}
+
+function term() {
+  return factor()
+    .then(() => {
+      return term1();
+    });
+}
+
+function firstTerm() {
+  return firstFactor()
+    .then(() => {
+      return term1();
+    });
+}
+
+function add() {
+  return match('+')
+    .then(() => {
+      return term();
+    })
+    .then(() => {
+      return popAdd();
+    });
+}
+
+function subtract() {
+  return match('-')
+    .then(() => {
+      return term();
+    })
+    .then(() => {
+      return popSub();
+    });
+}
+
+function expression() {
+  function expressionTail() {
+    let nextChar = look();
+    if (isAddop(nextChar)) {
+      return q()
+        .then(() => {
+          return push();
+        })
+        .then(() => {
+          let nextChar = look();
+          switch(nextChar) {
+            case '+': return add();
+            case '-': return subtract();
+          }
+        })
+        .then(() => {
+          return expressionTail();
+        });
+    }
+  }
+
+  return firstTerm()
+    .then(() => {
+      return expressionTail();
+    });
+}
+
+function doIf() {
+  let label1, label2;
+  return match('i')
+    .then(() => {
+      return boolExpression();
+    })
+    .then(() => {
+      label1 = newLabel();
+      label2 = label1;
+      return branchFalse(label1);
+    })
+    .then(() => {
+      return block();
+    })
+    .then(() => {
+      let nextChar = look();
+      if (nextChar === 'l') {
+        return match('l')
+          .then(() => {
+            label2 = newLabel();
+            return branch(label2);
+          })
+          .then(() => {
+            return postLabel(label1);
+          })
+          .then(() => {
+            return block();
+          });
+      }
+    })
+    .then(() => {
+      return postLabel(label2);
+    })
+    .then(() => {
+      return match('e');
+    });
+}
+
+function doWhile() {
+  let label1, label2;
+  return match('w')
+    .then(() => {
+      label1 = newLabel();
+      label2 = newLabel();
+      return postLabel(label1);
+    })
+    .then(() => {
+      return boolExpression();
+    })
+    .then(() => {
+      return branchFalse(label2);
+    })
+    .then(() => {
+      return block();
+    })
+    .then(() => {
+      return match('e');
+    })
+    .then(() => {
+      return branch(label1);
+    })
+    .then(() => {
+      return postLabel(label2);
+    });
 }
 
 init()
