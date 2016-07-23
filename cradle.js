a5cbc940707f86c273521d3e181620f26bd5faf5
Mike Lang <mike.lang1010@gmail.com>
2016-07-23 16:37:06 -0500

    Adjustments to handle input piped in from file

--- cradle.js
+++ cradle.js
@@ -4,44 +4,87 @@ const q = require('q');
 
 const TAB = '\t';
 
+const util = require('util');
+
 let look;
 exports.look = function() {
   return look;
 };
 
+let programText = '',
+  inputConsumed = false;
+
 function getChar() {
   const stdin = process.stdin;
-  const deferred = q.defer();
 
-  stdin.setRawMode(true);
-  stdin.resume();
-  stdin.once('data', function(c) {
-    if ( c.toString() === '\u0003' ) {
-      process.exit();
+  if (stdin.isTTY) {
+    // If STDIN is attached to a terminal,
+    // we can switch to raw mode and consume
+    // characters one at a time as follows.
+    //
+    // Otherwise, we'll stream everything to a
+    // buffer than then character by character
+    // iterate over the buffer.
+    const deferred = q.defer();
+    stdin.setRawMode(true);
+    stdin.resume();
+    stdin.once('data', function(c) {
+      if ( c.toString() === '\u0003' ) {
+        process.exit();
+      }
+      stdin.pause();
+      look = c.toString();
+
+      stdin.removeAllListeners('error');
+      return deferred.resolve();
+    });
+    stdin.once('error', function(err) {
+      return deferred.reject(err);
+    });
+
+    return deferred.promise;
+  } else {
+    var deferred = q.defer();
+    if (!inputConsumed) {
+      stdin.on('readable', function () {
+        let readData = stdin.read();
+
+        if (readData) {
+          programText += readData.toString();
+        }
+      });
+      stdin.on('end', function() {
+        inputConsumed = true;
+        if (!programText.length) {
+          process.exit();
+        }
+        look = programText[0];
+        programText = programText.slice(1);
+        deferred.resolve();
+      });
+    } else {
+      if (!programText.length) {
+        process.exit();
+      }
+      look = programText[0];
+      programText = programText.slice(1);
+      deferred.resolve();
     }
-    stdin.pause();
-    stdin.setRawMode(false);
-    look = c.toString();
-    stdin.removeAllListeners('error');
-    return deferred.resolve();
-  });
-  stdin.once('error', function(err) {
-    return deferred.reject(err);
-  });
-
-  return deferred.promise;
+    return deferred.promise;
+  }
 }
 exports.getChar = getChar;
 
 function error(err) {
   console.log('\nError: ' + err + '.');
   console.log(new Error().stack);
+  throw new Error(err);
 }
 exports.error = error;
 
 function abort(err) {
   error(err);
-  process.exit(1);
+  //process.exit(1);
 }
 exports.abort = abort;
 
